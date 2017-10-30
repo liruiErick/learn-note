44ba91d3fc550150bebeed07737abd39625f574d
baijunjie <625603381@qq.com>
2017-10-30 23:18:53 +0900

    update

---
+++ Web 学习笔记/程序代码片段/常用 js/ES5 用数组替代 Map.js
@@ -0,0 +1,36 @@
+﻿function hasBindValue(arr, obj) {
+    return arr.some(item => item[0] === obj);
+}
+
+function setBindValue(arr, obj, bindValue) {
+    if (!arr.some(item => {
+        if (item[0] === obj) {
+            item[1] = bindValue;
+            return true;
+        }
+    })) {
+        arr.push([obj, bindValue]);
+    }
+    return arr;
+}
+
+function getBindValue(arr, obj) {
+    let bindValue;
+    arr.some(item => {
+        if (item[0] === obj) {
+            bindValue = item[1];
+            return true;
+        }
+    });
+    return bindValue;
+}
+
+function deleteBindValue(arr, obj) {
+    arr.some((item, index) => {
+        if (item[0] === obj) {
+            arr.splice(index, 1);
+            return true;
+        }
+    });
+    return arr;
+}
