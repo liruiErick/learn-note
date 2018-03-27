f9b217cc882c62077fe1a315893478a0bc94ce2a
baijunjie <625603381@qq.com>
2018-03-28 01:18:40 +0900

    update

--- Web 学习笔记/后端学习笔记/php 学习笔记/WordPress/代码片段/bottom-fixed-area/_.php
+++ Web 学习笔记/后端学习笔记/php 学习笔记/WordPress/代码片段/bottom-fixed-area/_.php
@@ -1,7 +1,9 @@
 <div class="bottom-fixed-area">
     <div class="wrap">
         <div class="outer-box">
-            <div class="directory-btn only-mobile">目<br>录</div>
+            <div class="directory-btn only-mobile none">
+                <i class="iconfont icon-directory"></i>
+            </div>
             <div id="scrollTop" class="scroll-top box-shadow-thin"><i></i></div>
             <a class="authenticate-btn" href="<?php echo HOME_URL; ?>/contact/">
                 <div>
